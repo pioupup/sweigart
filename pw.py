9cf767c60a71cf367543356a68c8757f2fc95959
pioupup <pioupup@gmail.com>
2019-05-06 17:05:34 +0300

    Added pw.py 178page

---
+++ pw.py
@@ -0,0 +1,19 @@
+#! python3
+# pw.py - Программа для незащищенного хранения паролей
+
+PASSWORDS = {'email': 'sgdASGDasdfasg',
+			 'blog': '@42435',
+			 'luggage': '12345'}
+
+import sys, pyperclip
+if len(sys.argv) < 2:
+	print('Используйте: python pw.py [имя учетной записи] - копирование пароля учетной записи')
+	sys.exit()
+
+account = sys.argv[1]
+
+if account in PASSWORDS:
+	pyperclip.copy(PASSWORDS[account])
+	print('Пароль для ' + account + ' скопирован в буфер.')
+else:
+	print('Учетная запись ' + account + ' отсутсвует в списке.')
